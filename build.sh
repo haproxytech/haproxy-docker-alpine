20acdc66ef8bc2d9349a211bac6d0dc75f8bb645
Dinko Korunic <dkorunic@haproxy.com>
2019-09-26 10:08:24 +0000

    MAJOR: Complete image rework to start having automated Docker Hub builds.

--- build.sh
+++ build.sh
@@ -2,14 +2,16 @@
 set -e
 
 DOCKER_TAG="haproxytech/haproxy-alpine"
-HAPROXY_BRANCHES="1.6 1.7 1.8 1.9 2.0 2.1"
+HAPROXY_GITHUB_URL="https://github.com/haproxytech/haproxy-docker-alpine/blob/master"
+HAPROXY_BRANCHES="1.5 1.6 1.7 1.8 1.9 2.0 2.1"
 HAPROXY_CURRENT_BRANCH="2.0"
 PUSH="no"
+HAPROXY_UPDATED=""
 
 for i in $HAPROXY_BRANCHES; do
     echo "Building HAProxy $i"
 
-    DOCKERFILE="Dockerfile-$i"
+    DOCKERFILE="$i/Dockerfile"
     HAPROXY_MINOR_OLD=$(awk '/^ENV HAPROXY_MINOR/ {print $NF}' "$DOCKERFILE")
 
     ./update.sh "$i"
@@ -17,24 +19,58 @@ for i in $HAPROXY_BRANCHES; do
     HAPROXY_MINOR=$(awk '/^ENV HAPROXY_MINOR/ {print $NF}' "$DOCKERFILE")
 
     if [ "x$1" != "xforce" ]; then
-        if [ "x$HAPROXY_MINOR_OLD" = "x$HAPROXY_MINOR" ]; then
+        if [ "$HAPROXY_MINOR_OLD" = "$HAPROXY_MINOR" ]; then
             echo "No new releases, not building $i branch"
             continue
-        else
-            PUSH="yes"
         fi
-    else
-        PUSH="yes"
     fi
+
+    PUSH="yes"
+    HAPROXY_UPDATED="$HAPROXY_UPDATED $HAPROXY_MINOR"
 
     docker pull $(awk '/^FROM/ {print $2}' "$DOCKERFILE")
-    docker build -t "$DOCKER_TAG:$HAPROXY_MINOR" -f "$DOCKERFILE" .
+
+    docker build -t "$DOCKER_TAG:$HAPROXY_MINOR" "$i"
     docker tag "$DOCKER_TAG:$HAPROXY_MINOR" "$DOCKER_TAG:$i"
-    if [ "x$i" = "x$HAPROXY_CURRENT_BRANCH" ]; then
+
+    if [ "$i" = "$HAPROXY_CURRENT_BRANCH" ]; then
         docker tag "$DOCKER_TAG:$HAPROXY_MINOR" "$DOCKER_TAG:latest"
     fi
+
+    docker run -it --rm "$DOCKER_TAG:$HAPROXY_MINOR" /usr/local/sbin/haproxy -c -f /usr/local/etc/haproxy/haproxy.cfg
+
+    if git rev-parse "$HAPROXY_MINOR" >/dev/null 2>&1; then
+        git tag -d "$HAPROXY_MINOR" || true
+        git push origin ":$HAPROXY_MINOR" || true
+    fi
+    git tag "$HAPROXY_MINOR"
 done
 
-if [ "x$PUSH" = "xyes" ]; then
-    docker push "$DOCKER_TAG"
+if [ "$PUSH" = "no" ]; then
+        exit 0
 fi
+
+echo "# Supported tags and respective \`Dockerfile\` links\n" > README.md
+for i in $(awk '/^ENV HAPROXY_MINOR/ {print $NF}' */Dockerfile| sort -n -r); do
+        short=$(echo $i | cut -d. -f1-2 |cut -d- -f1)
+        if [ "$short" = "$HAPROXY_CURRENT_BRANCH" ]; then
+                if [ "$short" = "$i" ]; then
+                        final="-\t[\`$i\`, \`latest\`]($HAPROXY_GITHUB_URL/$short/Dockerfile)"
+                else
+                        final="-\t[\`$i\`, \`$short\`, \`latest\`]($HAPROXY_GITHUB_URL/$short/Dockerfile)"
+                fi
+        else
+                if [ "$short" = "$i" ]; then
+                        final="-\t[\`$i\`]($HAPROXY_GITHUB_URL/$short/Dockerfile)"
+                else
+                        final="-\t[\`$i\`, \`$short\`]($HAPROXY_GITHUB_URL/$short/Dockerfile)"
+                fi
+        fi
+        echo "$final" >> README.md
+done
+echo >> README.md
+cat README_short.md >> README.md
+
+git commit -a -m "Automated commit triggered by $HAPROXY_UPDATED release(s)"
+git push
+git push --tags
